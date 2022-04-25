ae4f93ba2332c6a36b38a144a245b0d4a3b32750
alzorriz <100152700+alzorriz@users.noreply.github.com>
2022-04-25 03:28:58 +0200

    Add files via upload

---
+++ grafica_comparativa.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f0 = np.loadtxt("rl002.f0", dtype=float)
+f0ref = np.loadtxt("rl002.f0ref", dtype=float)
+
+fm = 20000
+time = np.arange(0,len(f0)).astype(float)
+time = time/fm
+
+plt.subplot(211)
+plt.title("Parámetros de la señal")
+plt.plot(time, f0, linewidth =0.5)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('pitch calculado (Hz)')
+plt.grid(True)
+plt.subplot(212)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('pitch wavesurfer (Hz)')
+plt.plot(time, f0ref, linewidth =0.5)
+plt.grid(True)
+plt.show()
---
+++ grafica_pitch.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile as sf
+
+signal, fmuestreo=sf.read('rl002_sonoro.wav')
+time=np.arange(0,len(signal))/fmuestreo
+plt.subplot(2,1,1)
+plt.title("Análisis de pitch")
+plt.plot(time,signal)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Amplitud')
+plt.grid(True)
+
+plt.subplot(2,1,2)
+plt.xlabel('Muestras')
+plt.ylabel('Autocorrelación')
+plt.acorr(signal, maxlags=500)
+plt.grid(True)
+
+plt.show() 
